0ca5a99ebeca0927c033da120a58fa380521ee85
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2016-12-02 08:42:53 -0800

    add definition specs and methods for save, all, and clear. create definitionform erb page

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,9 +1,24 @@
 class Definition
   @@meaning = []
 
-define_method(:initialize) do |word, definition|
-  @word = word
+define_method(:initialize) do |definition|
   @definition = definition
 end
 
+
+  define_method(:definition) do
+    @definition
+  end
+
+  define_singleton_method(:all) do
+    @@meaning
+  end
+
+  define_singleton_method(:clear) do
+  @@meaning = []
+  end
+
+  define_method(:save) do
+  @@meaning.push(self)
+  end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,2 +1,29 @@
 require('rspec')
 require('definition')
+
+describe('Definition') do
+  before() do
+    Definition.clear()
+  end
+
+describe('#definition') do
+  it('returns the definition of the word') do
+    test_definition = Definition.new('any member of the kingdom Plantae')
+    expect(test_definition.definition()).to(eq('any member of the kingdom Plantae'))
+  end
+end
+
+describe('.all') do
+  it('is empty at first') do
+    expect(Definition.all()).to(eq([]))
+  end
+end
+
+describe('#save') do
+    it('definition to the array of saved definitions') do
+      test_definition = Definition.new('any member of the kingdom Plantae')
+      test_definition.save()
+      expect(Definition.all()).to(eq([test_definition]))
+    end
+  end
+end
